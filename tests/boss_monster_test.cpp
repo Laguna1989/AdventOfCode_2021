e0f61751bd7a749d3dfa5c990edafb571cebffdd
Semitalis <kerim.merdenoglu@gmail.com>
2021-12-17 16:43:39 +0100

    Added test values for Kerim

--- tests/boss_monster_test.cpp
+++ tests/boss_monster_test.cpp
@@ -4,10 +4,18 @@
 #include "update_transform.hpp"
 #include <gtest/gtest.h>
 
-TEST(BossMonsterTest, JustDoIt)
+TEST(BossMonsterTest, JustDoItSimon)
 {
     auto const commands = parse_input(massive_input_simon);
     auto const resulting_position = update_transform_consecutive(commands);
 
     ASSERT_EQ(resulting_position.horizontal * resulting_position.depth, 2086357770);
+}
+
+TEST(BossMonsterTest, JustDoItKerim)
+{
+    auto const commands = parse_input(massive_input_kerim);
+    auto const resulting_position = update_transform_consecutive(commands);
+
+    ASSERT_EQ(resulting_position.horizontal * resulting_position.depth, 1840311528);
 }
