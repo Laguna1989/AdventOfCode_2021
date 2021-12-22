461b4334fbbf23a63f7bee3baf819db4b6c02533
Simon Weis <laguna_1989@gmx.net>
2021-12-22 11:14:16 +0100

    make tests parametrized

--- day_03/test/calculate_rate_test.cpp
+++ day_03/test/calculate_rate_test.cpp
@@ -7,35 +7,34 @@ class CalculateRateParameterizedTestFixture
           std::pair<std::vector<Diagnostic_input_type>, unsigned long>> {
 };
 
-TEST_P(CalculateRateParameterizedTestFixture, CorrectGammaRate)
+using CalculateGammaRateParameterizedTestFixture = CalculateRateParameterizedTestFixture;
+
+TEST_P(CalculateGammaRateParameterizedTestFixture, CorrectGammaRate)
 {
     auto [gamma_rate, epsilon_rate] = calculate_rates(GetParam().first);
-
     ASSERT_EQ(gamma_rate, GetParam().second);
 }
 
-INSTANTIATE_TEST_SUITE_P(CalculateRateParameterizedTest, CalculateRateParameterizedTestFixture,
+INSTANTIATE_TEST_SUITE_P(CalculateGammaRateParameterizedTest,
+    CalculateGammaRateParameterizedTestFixture,
     ::testing::Values(
         std::make_pair(std::vector<Diagnostic_input_type> { 0b10010, 0b10010 }, 0b10010),
         std::make_pair(std::vector<Diagnostic_input_type> { 0b00100, 0b11110, 0b10110, 0b10111,
                            0b10101, 0b01111, 0b00111, 0b11100, 0b10000, 0b11001, 0b00010, 0b01010 },
             0b10110)));
 
-TEST(CalculateRateTest, CorrectEpsilonRate)
+using CalculateEpsilonRateParameterizedTestFixture = CalculateRateParameterizedTestFixture;
+
+TEST_P(CalculateEpsilonRateParameterizedTestFixture, CorrectEpsilonRate)
 {
-    std::vector<Diagnostic_input_type> const input { 0b10010, 0b10010 };
-
-    auto [gamma_rate, epsilon_rate] = calculate_rates(input);
-
-    ASSERT_EQ(epsilon_rate, 0b01101);
+    auto [gamma_rate, epsilon_rate] = calculate_rates(GetParam().first);
+    ASSERT_EQ(epsilon_rate, GetParam().second);
 }
 
-TEST(CalculateRateTest, CorrectEpsilonRate2)
-{
-    std::vector<Diagnostic_input_type> const input { 0b00100, 0b11110, 0b10110, 0b10111, 0b10101,
-        0b01111, 0b00111, 0b11100, 0b10000, 0b11001, 0b00010, 0b01010 };
-
-    auto [gamma_rate, epsilon_rate] = calculate_rates(input);
-
-    ASSERT_EQ(epsilon_rate, 0b01001);
-}
+INSTANTIATE_TEST_SUITE_P(CalculateEpsilonRateParameterizedTest,
+    CalculateEpsilonRateParameterizedTestFixture,
+    ::testing::Values(
+        std::make_pair(std::vector<Diagnostic_input_type> { 0b10010, 0b10010 }, 0b01101),
+        std::make_pair(std::vector<Diagnostic_input_type> { 0b00100, 0b11110, 0b10110, 0b10111,
+                           0b10101, 0b01111, 0b00111, 0b11100, 0b10000, 0b11001, 0b00010, 0b01010 },
+            0b01001)));
